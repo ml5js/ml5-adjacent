3a53f7d8faaeaa60eaea59b1b761dbb7573862ef
shiffman <daniel@shiffman.net>
2018-06-14 16:03:42 -0400

    simplfying style transfer

--- 04_StyleTransfer_Image/sketch.js
+++ 04_StyleTransfer_Image/sketch.js
@@ -1,12 +1,11 @@
 // Copyright (c) 2018 ml5
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 /* ===
-ML5 Example
-Fast_Style_Transfer_Simple
-Fast Style Transfer Simple Example with p5.js
+ml5 Example
+Style Transfer Image Example using p5.js
 This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young American Girl, The Dance)
 === */
 
@@ -14,9 +13,8 @@ let inputImg;
 let statusMsg;
 let transferBtn;
 
-// Create two Fast Style methods with different pre-trained models
-const style1 = new ml5.StyleTransfer('models/wave', modelLoaded);
-const style2 = new ml5.StyleTransfer('models/udnie', modelLoaded);
+// Create two Style methods with different pre-trained models
+const style1 = ml5.styleTransfer('models/wave', modelLoaded);
 
 function setup() {
   noCanvas();
@@ -34,7 +32,7 @@ function setup() {
 // A function to be called when the models have loaded
 function modelLoaded() {
   // Check if both models are loaded
-  if(style1.ready && style2.ready){
+  if (style1.ready) {
     statusMsg.html('Ready!')
   }
 }
@@ -42,12 +40,8 @@ function modelLoaded() {
 // Apply the transfer to both images!
 function transferImages() {
   statusMsg.html('Applying Style Transfer...!');
-
-  var styleA = style1.transfer(inputImg.elt);
-  createImg(styleA.src).parent('styleA');
-
-  var styleB = style2.transfer(inputImg.elt);
-  createImg(styleB.src).parent('styleB');
-
+  style1.transfer(inputImg, function(result) {
+    createImg(result.src).parent('style');
+  });
   statusMsg.html('Done!');
 }
