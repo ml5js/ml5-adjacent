04ddf1c7b0d5b06f0fed67b647d50ad3ec1afbb1
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-06-15 15:39:26 -0400

    ignore this commit, just need it for live testing

--- 03_LSTM_Text/sketch.js
+++ 03_LSTM_Text/sketch.js
@@ -1,76 +1,76 @@
-// Copyright (c) 2018 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
+// // Copyright (c) 2018 ml5
+// //
+// // This software is released under the MIT License.
+// // https://opensource.org/licenses/MIT
 
-/* ===
-ml5 Example
-LSTM Generator example with p5.js
-This uses a pre-trained model on a corpus of Nietschze
-=== */
+// /* ===
+// ml5 Example
+// LSTM Generator example with p5.js
+// This uses a pre-trained model on a corpus of Nietschze
+// === */
 
-// Create the LSTM Generator passing it the model directory
-const lstm = ml5.LSTMGenerator('models/zora_neale_hurston/', modelReady);
+// // Create the LSTM Generator passing it the model directory
+// const lstm = ml5.LSTMGenerator('models/zora_neale_hurston/', modelReady);
 
-let textInput;
-// let lengthSlider;
-// let tempSlider;
-let button;
+// let textInput;
+// // let lengthSlider;
+// // let tempSlider;
+// let button;
 
-function modelReady() {
-  select('#status').html('Model Loaded');
-}
+// function modelReady() {
+//   select('#status').html('Model Loaded');
+// }
 
-function setup() {
-  noCanvas();
+// function setup() {
+//   noCanvas();
 
-  // Grab the DOM elements
-  textInput = select('#textInput');
-  // lengthSlider = select('#lenSlider');
-  // tempSlider = select('#tempSlider');
-  button = select('#generate');
+//   // Grab the DOM elements
+//   textInput = select('#textInput');
+//   // lengthSlider = select('#lenSlider');
+//   // tempSlider = select('#tempSlider');
+//   button = select('#generate');
 
-  // DOM element events
-  button.mousePressed(generate);
-  // lengthSlider.input(updateSliders);
-  // tempSlider.input(updateSliders);
+//   // DOM element events
+//   button.mousePressed(generate);
+//   // lengthSlider.input(updateSliders);
+//   // tempSlider.input(updateSliders);
 
-  // Update the slider values
-  // function updateSliders() {
-  //   select('#length').html(lengthSlider.value());
-  //   select('#temperature').html(tempSlider.value());
-  // }
-}
+//   // Update the slider values
+//   // function updateSliders() {
+//   //   select('#length').html(lengthSlider.value());
+//   //   select('#temperature').html(tempSlider.value());
+//   // }
+// }
 
-// Generate new text
-function generate() {
-  // Update the status log
-  select('#status').html('Generating...');
+// // Generate new text
+// function generate() {
+//   // Update the status log
+//   select('#status').html('Generating...');
 
-  // Grab the original text
-  let original = textInput.value();
-  // Make it to lower case
-  let txt = original.toLowerCase();
+//   // Grab the original text
+//   let original = textInput.value();
+//   // Make it to lower case
+//   let txt = original.toLowerCase();
 
-  // Check if there's something to send
-  if (txt.length > 0) {
-    // This is what the LSTM generator needs
-    // Seed text, temperature, length to outputs
-    // TODO: What are the defaults?
-    let data = {
-      seed: txt,
-      temperature: 0.5,
-      length: 50
-    };
+//   // Check if there's something to send
+//   if (txt.length > 0) {
+//     // This is what the LSTM generator needs
+//     // Seed text, temperature, length to outputs
+//     // TODO: What are the defaults?
+//     let data = {
+//       seed: txt,
+//       temperature: 0.5,
+//       length: 50
+//     };
 
-    // Generate text with the lstm
-    lstm.generate(data, gotData);
+//     // Generate text with the lstm
+//     lstm.generate(data, gotData);
 
-    // When it's done
-    function gotData(result) {
-      // Update the status log
-      select('#status').html('Ready!');
-      select('#result').html(txt + result.generated);
-    }
-  }
-}
+//     // When it's done
+//     function gotData(result) {
+//       // Update the status log
+//       select('#status').html('Ready!');
+//       select('#result').html(txt + result.generated);
+//     }
+//   }
+// }
